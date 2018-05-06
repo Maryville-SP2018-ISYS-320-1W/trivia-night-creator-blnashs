9ed3934cb662a6cfbcf99f8d446df7ac17c29f5e
Barbara Nash <bnash1@live.maryville.edu>
2018-05-06 16:46:59 -0500

    string

--- src/edu/maryville/isys320/TriviaNight/BuildTriviaNightPresentation.java
+++ src/edu/maryville/isys320/TriviaNight/BuildTriviaNightPresentation.java
@@ -1,5 +1,4 @@
 package edu.maryville.isys320.TriviaNight;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,4 +15,60 @@ public class BuildTriviaNightPresentation {
 
 	}
 	
+}
+
+
+String [ ] Categories = new String [ ] {“History”, “Sports”, “State” “Names”};
+Arrays.sorts(Categories);
+Int I = 0
+For(String temp: Categories){
+System.out.println(“Categories” + i++ + “:” + temp);
+}
+}
+}
+
+
+Public String getBar (int i)
+Throws AlsCustomException
+{
+If (i == 0)
+{
+//throw our custom exception
+Throw new AlsCustomException(“Please hit return…”);
+}
+Else
+{
+Return “Thanks”;
+}
+}
+}
+
+
+Set<String1> words = new HashSet<String1>( );
+Scanner in = new Scanner(new File(“categories.txt”));
+While (in.hasNext( ) ) {
+String question = in.next();
+question.add(question);
+System.out.println(“History” = + question());
+}
+}
+}
+Set<String2> words = new HashSet<String2>( );
+Scanner in = new Scanner(new File(“categories.txt”));
+While (in.hasNext( ) ) {
+String question = in.next();
+question.add(question);
+System.out.println(“Sports” = + question());
+}
+}
+}
+
+Set<String3> words = new HashSet<String3>( );
+Scanner in = new Scanner(new File(“categories.txt”));
+While (in.hasNext( ) ) {
+String question = in.next();
+question.add(question);
+System.out.println(“State Names” = + question());
+}
+}
 }
